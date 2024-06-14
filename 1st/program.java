bc889556b3937cf4fe268973e27bd47600f138e0
Mohammed-Alnahdi <m.kh.alnahdi@gmail.com>
2024-06-14 21:58:28 +0300

    Solve First Exercise

---
+++ 1st/program.java
@@ -0,0 +1,12 @@
+/**
+ * Name: Mohammed Khalid Alnahdi.
+ * Crated at: 14-june-2024 Friday.
+ */
+
+class PrintNameProgram {
+    public static void main(String[] args) {
+
+        System.out.println("Hello\nMohammed Alnahdi");
+
+    }
+}
